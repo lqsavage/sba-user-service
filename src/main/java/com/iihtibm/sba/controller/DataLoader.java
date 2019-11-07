a7c792f8d2ea1f459ad8297aefd0883e4cdc1e15
lqsavage <lqsavage@163.com>
2019-11-07 11:03:40 +0800

    Update DataLoader.java

--- src/main/java/com/iihtibm/sba/controller/DataLoader.java
+++ src/main/java/com/iihtibm/sba/controller/DataLoader.java
@@ -48,5 +48,47 @@ public class DataLoader implements ApplicationRunner {
 			return newUser;
 		});
 
+		userRepository.findByName("mentor01@163.com").map(oldUser -> {
+			logger.warn("mentor_01 user already exist");
+			return oldUser;
+		}).orElseGet(() -> {
+			User newUser = new User();
+			newUser.setId(sequencegenerator.generateSequence(User.SEQUENCE_NAME));
+			newUser.setUserName("mentor01@163.com");
+			newUser.setPassword("$2a$04$j5Px7Uk5a/sgJcHMlBkGk.GH0tFSrPpfn5VqA5MByr8vBWdN4qU3a");
+			newUser.setFirstName("mentor_01");
+			newUser.setLastName("LI");
+			newUser.setContactNumber(9434580584L);
+			newUser.setRole("MENTOR");
+			newUser.setYearsOfExperience((float) 20);
+			newUser.setLinkedinUrl("mentor01@163.com");
+			newUser.setActive(true);
+			newUser.setConfirmedSignUp(true);
+			newUser.setPersistent(true); // to create created_date
+			userRepository.save(newUser);
+			logger.info("mentor_01 user inserted");
+			return newUser;
+		});
+
+		userRepository.findByName("user01@163.com").map(oldUser -> {
+			logger.warn("user_01 user already exist");
+			return oldUser;
+		}).orElseGet(() -> {
+			User newUser = new User();
+			newUser.setId(sequencegenerator.generateSequence(User.SEQUENCE_NAME));
+			newUser.setUserName("user01@163.com");
+			newUser.setPassword("$2a$04$j5Px7Uk5a/sgJcHMlBkGk.GH0tFSrPpfn5VqA5MByr8vBWdN4qU3a");
+			newUser.setFirstName("user_01");
+			newUser.setLastName("GU");
+			newUser.setContactNumber(9434580584L);
+			newUser.setRole("USER");
+			newUser.setActive(true);
+			newUser.setConfirmedSignUp(true);
+			newUser.setPersistent(true); // to create created_date
+			userRepository.save(newUser);
+			logger.info("user_01 user inserted");
+			return newUser;
+		});
+
 	}
 }
